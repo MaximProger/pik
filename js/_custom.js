e8480cfad702a8ce4c5f86c92425b131f2334e95
Maksim Koshman <maksimkoshman090@gmail.com>
2020-07-03 10:02:19 +0300

    add modal windows

--- js/_custom.js
+++ js/_custom.js
@@ -73,4 +73,46 @@ $(document).ready(function () {
     $(this).toggleClass("active");
     $(".nav").toggleClass("active");
   });
+
+  // Модальное окно вход/регистрация
+  $("#linkLogin").on("click", function () {
+    $(".modal__link").removeClass("active");
+    $(".modal__content__body").removeClass("active");
+    $("#contentLogin").addClass("active");
+    $(this).addClass("active");
+  });
+
+  $("#linkRegistr").on("click", function () {
+    $(".modal__link").removeClass("active");
+    $(".modal__content__body").removeClass("active");
+    $("#contentRegistration").addClass("active");
+    $(this).addClass("active");
+  });
+
+  $("#loginBtn").on("click", function () {
+    $("#modalLogin").addClass("active");
+  });
+
+  $(".openCabinet").on("click", function () {
+    $("#modalCabinet").addClass("active");
+  });
+
+  // Закрытие модальных окон
+  $(document).keyup(function (e) {
+    if (e.key === "Escape") {
+      $(".modal__wrapper").removeClass("active");
+    }
+  });
+
+  $(document).mouseup(function (e) {
+    let modalctr = $(".modal__wrapper");
+    let modal = $(".modal");
+    if (!modal.is(e.target) && modal.has(e.target).length === 0) {
+      modalctr.removeClass("active");
+    }
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".modal__wrapper").removeClass("active");
+  });
 });
